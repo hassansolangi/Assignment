fbd1eb7ff6a647e31c76522d611d65ba06deee47
Hassan Solangi <hassansolangi2021@gmail.com>
2022-05-27 15:51:19 +0500

    Assignment Submitted

---
+++ AssignmentSE/src/yes/RollNumber.java
@@ -0,0 +1,18 @@
+package yes;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class RollNumber {
+	@Test
+	public void Roll() {
+		int roll = 37;
+		assertEquals(37,roll);
+		}
+	
+		
+		
+		 
+	}
+
